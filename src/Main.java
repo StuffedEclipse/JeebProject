a915715679479b5d20d7a68641c4e88bbdb743eb
Trey Pattillo <33559758+StuffedEclipse@users.noreply.github.com>
2026-03-23 20:18:37 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,15 @@
+import game.Game;
 
+/**
+ * Creates a game of chess, runs it, and loops it until the game ends
+ */
+public class Main {
+    /**
+     * Starts up the game
+     */
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+        game.play();
+    }
+}
